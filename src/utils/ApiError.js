3346e66ff44d5eb8911ea6cf92d9c481fb303ae6
M-HamzaSiddiqui <userhamza01@gmail.com>
2024-03-10 23:01:13 +0530

    Typo fix in ApiError.js

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -15,7 +15,7 @@ class ApiError extends Error {
         if (stack) {
             this.stack = stack;
         } else {
-            Error.captureStackTrace(this.this.contructor);
+            Error.captureStackTrace(this.contructor);
         }
     }
 }
