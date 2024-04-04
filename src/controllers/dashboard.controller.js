91a02e97f2e504b7f29fa8cd9549cd2a6e2ce6dc
M-HamzaSiddiqui <userhamza01@gmail.com>
2024-04-05 00:30:10 +0530

    wrote aggregation pipeline for allChannelVideos

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,5 +1,5 @@
 
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { Like } from "../models/like.model.js";
@@ -10,13 +10,13 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
-    const channelId = req.user?._id
+    const channelId = req.user?._id;
 
     const totalVideos = await Video.countDocuments({
         owner: channelId
-    })
+    });
 
-    console.log(totalVideos, "\n")
+    console.log(totalVideos, "\n");
 
     const totalViews = await Video.aggregate([
         {
@@ -38,15 +38,15 @@ const getChannelStats = asyncHandler(async (req, res) => {
                 _id: 0
             }
         }
-    ])
+    ]);
 
-    console.log(totalViews, "\n")
+    console.log(totalViews, "\n");
 
     const totalSubscribers = await Subscription.countDocuments({
         channel: channelId
-    })
+    });
 
-    console.log(totalSubscribers, "\n")
+    console.log(totalSubscribers, "\n");
 
     const totalLikes = await Video.aggregate([
         {
@@ -65,21 +65,88 @@ const getChannelStats = asyncHandler(async (req, res) => {
         {
             $count: "likes"
         }
-    ])
+    ]);
 
-    console.log(totalLikes, "\n")
+    console.log(totalLikes, "\n");
 
     return res.json(new ApiResponse(200, {
         totalVideos,
         totalViews: totalViews[0].totalViews,
         totalSubscribers,
         totalLikes: totalLikes[0].likes
-    }, "success"))
+    }, "success"));
 
-}); 
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+
+    const channelId = req.user?._id;
+    const { page = 1, limit = 2 } = req.query;
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel Id");
+    }
+
+    const pipeline = [
+        {
+            $match: {
+                owner: channelId
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                foreignField: "_id",
+                localField: "owner",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullname: 1,
+                            avatar: 1,
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                owner: {
+                    $first: "$owner"
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                videofile: 1,
+                thumbnail: 1,
+                title: 1,
+                duration: 1,
+                views: 1,
+                owner: 1
+            }
+        }
+    ];
+
+    const options = {
+        page: parseInt(page),
+        limit: parseInt(limit)
+    };
+
+    const videos = Video.aggregate(pipeline);
+
+    const paginatedVideos = await Video.aggregatePaginate(videos, options)
+    console.log(paginatedVideos.docs)
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, paginatedVideos.docs, "Videos retrieved successfully")
+        );
+
 });
 
 export {
